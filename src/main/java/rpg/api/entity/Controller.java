52a2cb5a242cff4defe225a4004e12a8fe063a52
Griszder <griszder@gmail.com>
2018-09-20 15:54:59 +0200

    Added Controller setters

--- src/main/java/rpg/api/entity/Controller.java
+++ src/main/java/rpg/api/entity/Controller.java
@@ -1,11 +1,20 @@
 package rpg.api.entity;
 
+import java.util.UUID;
+
 import rpg.api.Direction;
 import rpg.api.Location;
+import rpg.api.Vec2D;
 
 public abstract class Controller {
+	
+	public abstract void setDisplayName(String displayName);
 	
 	public abstract void setLocation(Location location);
 	
 	public abstract void setLookingDirection(Direction direction);
+	
+	public abstract void setVelocity(Vec2D vector);
+	
+	public abstract void setUniqueId(UUID uuid); 
 }
