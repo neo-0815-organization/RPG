a1bf6c64b00cfbae60ba0a1dbfe2a4403f2bea99
neo_0815 <neo_0815@gmx.de>
2018-09-20 15:39:29 +0200

    Added "Controller"-Basics

--- src/main/java/rpg/api/entity/Controller.java
+++ src/main/java/rpg/api/entity/Controller.java
@@ -1,5 +1,11 @@
 package rpg.api.entity;
 
+import rpg.api.Direction;
+import rpg.api.Location;
+
 public abstract class Controller {
 	
+	public abstract void setLocation(Location location);
+	
+	public abstract void setLookingDirection(Direction direction);
 }
