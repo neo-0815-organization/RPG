cb17f87cff42f91d74128c4071abee97f2d0d828
neo_0815 <neo_0815@gmx.de>
2018-09-27 15:50:48 +0200

    Update Event.java

--- src/main/java/rpg/api/eventhandling/events/Event.java
+++ src/main/java/rpg/api/eventhandling/events/Event.java
@@ -3,9 +3,5 @@ package rpg.api.eventhandling.events;
 import rpg.api.eventhandling.EventType;
 
 public abstract class Event {
-	private final EventType eventType = EventType.EVENT;
-	
-	public EventType getEventType() {
-		return eventType;
-	}
+	public abstract EventType getEventType();
 }
