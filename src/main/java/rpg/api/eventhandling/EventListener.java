ef5ced13d31f9278ae21f15a3362ee1411adbdc7
neo_0815 <neo_0815@gmx.de>
2018-09-27 15:51:28 +0200

    Update EventListener.java

--- src/main/java/rpg/api/eventhandling/EventListener.java
+++ src/main/java/rpg/api/eventhandling/EventListener.java
@@ -3,11 +3,8 @@ package rpg.api.eventhandling;
 import rpg.api.eventhandling.events.Event;
 
 public abstract class EventListener {
-	EventType eventType;
 	
-	public EventType getEventType() {
-		return eventType;
-	}
+	public abstract EventType getEventType();
 	
 	public abstract void onEvent(final Event event);
 }
