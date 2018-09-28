0060a36fedf018fc622f3fa9cc39c581cd4b227f
Timcat41 <timcat.tl@gmail.com>
2018-09-28 22:16:46 +0200

    Update RPG.java

--- src/main/java/rpg/RPG.java
+++ src/main/java/rpg/RPG.java
@@ -7,44 +7,6 @@ import rpg.api.eventhandling.events.Event;
 
 public class RPG {
 	public static void main(final String[] args) {
-		final EventHandler h = new EventHandler();
 		
-		h.registerEventListener(new EventListener() {
-			@Override
-			public EventType getEventType() {
-				return EventType.TEST_EVENT;
-			}
-			
-			@Override
-			public void onEvent(final Event event) {
-				System.out.println("test_event");
-			}
-		});
-		
-		h.registerEventListener(new EventListener() {
-			@Override
-			public EventType getEventType() {
-				return EventType.GENERAL_EVENT;
-			}
-			
-			@Override
-			public void onEvent(final Event event) {
-				System.out.println("general_event");
-			}
-		});
-		
-		h.handle(new Event() {
-			@Override
-			public EventType getEventType() {
-				return EventType.TEST_EVENT;
-			}
-		});
-		
-		h.handle(new Event() {
-			@Override
-			public EventType getEventType() {
-				return EventType.GENERAL_EVENT;
-			}
-		});
 	}
 }
