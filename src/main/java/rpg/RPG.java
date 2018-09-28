d437b79c70eeb1c9f06aa9363db519589af2b336
Timcat41 <timcat.tl@gmail.com>
2018-09-28 22:17:06 +0200

    Update RPG.java

--- src/main/java/rpg/RPG.java
+++ src/main/java/rpg/RPG.java
@@ -1,10 +1,5 @@
 package rpg;
 
-import rpg.api.eventhandling.EventHandler;
-import rpg.api.eventhandling.EventListener;
-import rpg.api.eventhandling.EventType;
-import rpg.api.eventhandling.events.Event;
-
 public class RPG {
 	public static void main(final String[] args) {
 		
