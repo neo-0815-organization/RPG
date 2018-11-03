e13c247399bc168375c1d854c2d3678507f800c4
neo_0815 <neo_0815@gmx.de>
2018-11-03 22:47:48 +0100

    Edited type of 'Vec2D'

--- src/main/java/rpg/api/collision/Hitbox.java
+++ src/main/java/rpg/api/collision/Hitbox.java
@@ -10,13 +10,10 @@ import rpg.api.vector.Vec2D;
  */
 public abstract class Hitbox {
 	/**
-	 * The array of points represented as a
-	 * {@link Vec2D} specifying this {@link Hitbox}.
-	 * <code>points[0]</code> is measured relative to the {@link Entity} or
-	 * {@link Tile} being
-	 * boxed by this {@link Hitbox}, while <code>points[n] | n > 0</code> is
-	 * measured relative to
-	 * the {@link Vec2D}
+	 * The array of points represented as a {@link Vec2D} specifying this
+	 * {@link Hitbox}. <code>points[0]</code> is measured relative to the
+	 * {@link Entity} or {@link Tile} being boxed by this {@link Hitbox}, while
+	 * <code>points[n] | n > 0</code> is measured relative to the {@link Vec2D}
 	 * <code>points[0]</code>.
 	 */
 	private final UnmodifiableVec2D[] points;
@@ -29,13 +26,14 @@ public abstract class Hitbox {
 	 * Checks weather this {@link Hitbox} collides with an other {@link Hitbox}.
 	 * 
 	 * @param colliderHitbox
-	 *     the {@link Hitbox} to check for collision
+	 *            the {@link Hitbox} to check for collision
 	 * @param colliderPosition
-	 *     the position of the {@link Entity} or {@link Tile} the {@link Hitbox}
-	 *     <code>colliderHitbox</code> represents, relative to the position of the
-	 *     {@link Entity} or {@link Tile} represented by this {@link Hitbox}
+	 *            the position of the {@link Entity} or {@link Tile} the
+	 *            {@link Hitbox} <code>colliderHitbox</code> represents,
+	 *            relative to the position of the {@link Entity} or {@link Tile}
+	 *            represented by this {@link Hitbox}
 	 * @return <code>true</code> if this {@link Hitbox} collides with the
-	 * {@link Hitbox} <code>colliderHitbox</code>
+	 *         {@link Hitbox} <code>colliderHitbox</code>
 	 */
 	public abstract boolean checkCollision(Hitbox colliderHitbox, UnmodifiableVec2D colliderPosition);
 	
@@ -43,13 +41,14 @@ public abstract class Hitbox {
 	 * Checks weather this {@link Hitbox} collides with a point.
 	 * 
 	 * @param colliderPoint
-	 *     the position of a point, relative to the position of the {@link Entity}
-	 *     or {@link Tile} represented by this {@link Hitbox}, to check for
-	 *     collision, represented as a {@link Vec2D}
-	 * @return <code>true</code> if this {@link Hitbox} collides with the
-	 * point <code>colliderPoint</code>
+	 *            the position of a point, relative to the position of the
+	 *            {@link Entity} or {@link Tile} represented by this
+	 *            {@link Hitbox}, to check for collision, represented as a
+	 *            {@link Vec2D}
+	 * @return <code>true</code> if this {@link Hitbox} collides with the point
+	 *         <code>colliderPoint</code>
 	 */
-	public abstract boolean checkCollision(UnmodifiableVec2D colliderPoint);
+	public abstract boolean checkCollision(Vec2D<?> colliderPoint);
 	
 	protected UnmodifiableVec2D getPoint(final int i) {
 		return points[i];
--- src/main/java/rpg/api/collision/TriangleHitbox.java
+++ src/main/java/rpg/api/collision/TriangleHitbox.java
@@ -1,29 +1,19 @@
 package rpg.api.collision;
 
 import rpg.api.vector.UnmodifiableVec2D;
+import rpg.api.vector.Vec2D;
 
 public class TriangleHitbox extends Hitbox {
+	
 	public TriangleHitbox(final UnmodifiableVec2D pointA, final UnmodifiableVec2D vecV, final UnmodifiableVec2D vecW) {
 		super(pointA, vecV, vecW);
 	}
 	
 	@Override
-	public boolean checkCollision(final UnmodifiableVec2D point) {
-		final UnmodifiableVec2D p = point.subtract(getVecA());
+	public boolean checkCollision(final Vec2D<?> point) {
+		final UnmodifiableVec2D p = point.subtract(getVecA()).toUnmodifiable();
 		
-		final double x = p.getX().getValueTiles(),
-				y = p.getY().getValueTiles(),
-				
-				vx = getVecV().getX().getValueTiles(),
-				vy = getVecV().getY().getValueTiles(),
-				
-				wx = getVecW().getX().getValueTiles(),
-				wy = getVecW().getY().getValueTiles(),
-				
-				den = vx * wy - wx * vy,
-				
-				q = (x * wy - wx * y) / den,
-				r = (vx * y - x * vy) / den;
+		final double x = p.getX().getValueTiles(), y = p.getY().getValueTiles(), vx = getVecV().getX().getValueTiles(), vy = getVecV().getY().getValueTiles(), wx = getVecW().getX().getValueTiles(), wy = getVecW().getY().getValueTiles(), den = vx * wy - wx * vy, q = (x * wy - wx * y) / den, r = (vx * y - x * vy) / den;
 		
 		return q > 0 && r > 0 && q + r < 1;
 	}
