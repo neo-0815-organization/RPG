b8e67641f2fc0f26641ada4bf28abf616ce34498
neo_0815 <neo_0815@gmx.de>
2018-12-13 20:59:45 +0100

    Update Image.java

--- src/main/java/rpg/worldcreator/Image.java
+++ src/main/java/rpg/worldcreator/Image.java
@@ -4,9 +4,10 @@ import java.awt.image.BufferedImage;
 
 public class Image {
 	private BufferedImage image;
-	private int id, xShift, yShift, rotation;
+	private int id, xShift, yShift;
+	private Rotation rotation;
 	
-	public Image(BufferedImage image, int xShift, int yShift, int rotation) {
+	public Image(BufferedImage image, int xShift, int yShift, Rotation rotation) {
 		this.image = image;
 		id = image != null ? RPGWorldCreator.getTextures().getFirst(RPGWorldCreator.getTextures().keyWithValueTwo(image)) : -1;
 		this.xShift = xShift;
@@ -14,7 +15,7 @@ public class Image {
 		this.rotation = rotation;
 	}
 	
-	public Image(int id, int xShift, int yShift, int rotation) {
+	public Image(int id, int xShift, int yShift, Rotation rotation) {
 		image = id < 0 ? RPGWorldCreator.getTextures().getSecond(RPGWorldCreator.getTextures().keyWithValueOne(id)) : null;
 		this.id = id < -1 ? -1 : id;
 		this.xShift = xShift;
@@ -38,17 +39,17 @@ public class Image {
 		return yShift;
 	}
 	
-	public int getRotation() {
+	public Rotation getRotation() {
 		return rotation;
 	}
 	
-	public void setRotation(int rotation) {
+	public void setRotation(Rotation rotation) {
 		this.rotation = rotation;
 	}
 	
 	public BufferedImage getImage() {
 		if(image == null) return null;
 		
-		return RPGWorldCreator.rotateImage(image.getSubimage(xShift, yShift, 32, 32), rotation);
+		return RPGWorldCreator.rotateImage(image.getSubimage(xShift, yShift, 32, 32), rotation.getAngle());
 	}
 }
