0f7d1713d9e3d69aa4fd925895a6c4141b4a9fe7
Timcat41 <timcat.tl@gmail.com>
2018-12-13 14:44:07 +0100

    fixed multi execution bug in keysensor

--- src/main/java/rpg/api/listener/key/KeyboardSensor.java
+++ src/main/java/rpg/api/listener/key/KeyboardSensor.java
@@ -2,16 +2,27 @@ package rpg.api.listener.key;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.HashMap;
 
 public class KeyboardSensor extends KeyAdapter {
+	private static final byte PRESSED = 0, RELEASED = 1;
+	private HashMap<Integer, Byte> keyAction = new HashMap<>();
 	
 	@Override
 	public void keyPressed(final KeyEvent e) {
-		KeyboardListener.setState(e.getKeyCode(), KeyState.PRESSING);
+		if(keyAction.get(e.getKeyCode()) != PRESSED) {
+			KeyboardListener.setState(e.getKeyCode(), KeyState.PRESSING);
+			
+			keyAction.put(e.getKeyCode(), PRESSED);
+		}
 	}
 	
 	@Override
 	public void keyReleased(final KeyEvent e) {
-		KeyboardListener.setState(e.getKeyCode(), KeyState.RELEASING);
+		if(keyAction.get(e.getKeyCode()) != RELEASED) {
+			KeyboardListener.setState(e.getKeyCode(), KeyState.RELEASING);
+			
+			keyAction.put(e.getKeyCode(), RELEASED);
+		}
 	}
 }
