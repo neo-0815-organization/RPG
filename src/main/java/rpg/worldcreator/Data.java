98509f0c27c87dccb61ccfb92b31c39138c8e40d
neo_0815 <neo_0815@gmx.de>
2018-12-29 22:30:12 +0100

    Fixed flood fill of 'WorldCreator'

--- src/main/java/rpg/worldcreator/Data.java
+++ src/main/java/rpg/worldcreator/Data.java
@@ -4,12 +4,12 @@ import java.awt.Point;
 
 public class Data {
 	// @formatter:off
-	public static final String version = "1.010";
+	public static final String version = "1.011";
 	public static final int tileSize = 32;
 	public static final Point[] fillOffsets = new Point[] {
-		new Point(-1, -1),	new Point(0, -1),	new Point(1, -1),
+							new Point(0, -1),
 		new Point(-1, 0),						new Point(1, 0),
-		new Point(-1, 1),	new Point(0, 1),	new Point(1, 1)
+							new Point(0, 1)
 	};
 	// @formatter:on
 }
