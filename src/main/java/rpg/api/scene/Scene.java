c346b2eb461444e2b6dc8eec4d7e7d1a0515a7af
neo_0815 <neo_0815@gmx.de>
2019-01-17 20:59:33 +0100

    Removed childish comment
    Erik's childish comment has been removed

--- src/main/java/rpg/api/scene/Scene.java
+++ src/main/java/rpg/api/scene/Scene.java
@@ -4,7 +4,6 @@ import rpg.api.gfx.IDrawable;
 
 /**
  * @author Erik Diers
- * <a href = "https://www.home24.de/kategorie/led-lampen-shop/"> Aditional infomation</a>
  */
 public abstract class Scene implements IDrawable {
 	
