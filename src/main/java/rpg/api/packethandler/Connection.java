c373925142803a1639f9a3e05dde9418c2a1c4eb
neo_0815 <neo_0815@gmx.de>
2019-03-15 23:00:21 +0100

    Fixed package sending of ServerSystem

--- src/main/java/rpg/api/packethandler/Connection.java
+++ src/main/java/rpg/api/packethandler/Connection.java
@@ -14,22 +14,23 @@ import rpg.api.packethandler.packet.time.PacketPong;
  * @author Neo Hornberger
  */
 public abstract class Connection extends PacketRegistry {
-	private final Connection	INSTANCE	= this;
-	private final OutputStream	out;
+	private final Connection INSTANCE = this;
+	private final OutputStream out;
 	
-	private ListeningThread	listeningThread;
-	private long			millis;
-	private int				ping;
+	private ListeningThread listeningThread;
+	private long millis;
+	private int ping;
 	
 	public Connection(final OutputStream out) {
 		this.out = out;
 	}
 	
 	/**
-	 * Will be evaluated when the {@link Connection} has received a {@link Packet}.
+	 * Will be evaluated when the {@link Connection} has received a
+	 * {@link Packet}.
 	 *
 	 * @param packet
-	 *     the received {@link Packet}
+	 *            the received {@link Packet}
 	 */
 	protected abstract void onPacketReceived(Packet packet);
 	
@@ -39,15 +40,16 @@ public abstract class Connection extends PacketRegistry {
 	
 	/**
 	 * Sends the {@link Packet} 'packet' with the {@link Object}[] 'objects' as
-	 * arguments to the {@link OutputStream} of this {@link Connection} instance.
+	 * arguments to the {@link OutputStream} of this {@link Connection}
+	 * instance.
 	 *
 	 * @param packet
-	 *     - the {@link Packet} that will be sent
+	 *            - the {@link Packet} that will be sent
 	 * @param objects
-	 *     - the arguments which will be used to initialize the {@link Packet}
-	 *     'packet'
+	 *            - the arguments which will be used to initialize the
+	 *            {@link Packet} 'packet'
 	 * @throws IOException
-	 *     if an I/O error occures
+	 *             if an I/O error occures
 	 */
 	protected void sendPacket(final Packet packet, final Object... objects) throws IOException {
 		if(objects == null || objects.length == 0) packet.init();
@@ -70,21 +72,21 @@ public abstract class Connection extends PacketRegistry {
 	 * Sends the {@link Packet} 'packet' with {@code null} as arguments.
 	 *
 	 * @param packet
-	 *     - the {@link Packet} that will be sent
+	 *            - the {@link Packet} that will be sent
 	 * @throws IOException
-	 *     if an I/O error occures
+	 *             if an I/O error occures
 	 * @see #sendPacket(Packet, Object...)
 	 */
 	protected void sendPacket(final Packet packet) throws IOException {
-		sendPacket(packet, out, null);
+		sendPacket(packet);
 	}
 	
 	protected void pingTest() {
 		millis = System.currentTimeMillis();
 		
 		try {
-			sendPacket(getPacket(-1, 0), out, millis);
-		} catch(final IOException e) {
+			sendPacket(getPacket(-1, 0), millis);
+		}catch(final IOException e) {
 			e.printStackTrace();
 		}
 	}
@@ -102,8 +104,8 @@ public abstract class Connection extends PacketRegistry {
 	 * Initializes the {@link ListeningThread} with the {@link Socket} 'socket'.
 	 *
 	 * @param socket
-	 *     the {@link Socket} which will be used to construct the
-	 *     {@link ListeningThread}
+	 *            the {@link Socket} which will be used to construct the
+	 *            {@link ListeningThread}
 	 */
 	protected void initListeningThread(final Socket socket) throws IOException {
 		if(listeningThread == null) listeningThread = new ListeningThread(socket) {
@@ -116,15 +118,15 @@ public abstract class Connection extends PacketRegistry {
 	}
 	
 	/**
-	 * Initializes the {@link ListeningThread} with the {@link InputStream} 'in' and
-	 * the {@link OutputStream} 'out'.
+	 * Initializes the {@link ListeningThread} with the {@link InputStream} 'in'
+	 * and the {@link OutputStream} 'out'.
 	 *
 	 * @param in
-	 *     the {@link InputStream} which will be used to construct the
-	 *     {@link ListeningThread}
+	 *            the {@link InputStream} which will be used to construct the
+	 *            {@link ListeningThread}
 	 * @param out
-	 *     the {@link OutputStream} which will be used to construct the
-	 *     {@link ListeningThread}
+	 *            the {@link OutputStream} which will be used to construct the
+	 *            {@link ListeningThread}
 	 */
 	protected void initListeningThread(final InputStream in, final OutputStream out) {
 		if(listeningThread == null) listeningThread = new ListeningThread(in, out) {
