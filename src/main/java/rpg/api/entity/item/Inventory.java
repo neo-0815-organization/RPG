4ab77c1a09b8f23860d0a8c2eadca9549ef1b397
Timcat41 <timcat.tl@gmail.com>
2019-04-04 22:24:03 +0200

    implemented questItem slots and armory / weaponry slots

--- src/main/java/rpg/api/entity/item/Inventory.java
+++ src/main/java/rpg/api/entity/item/Inventory.java
@@ -12,10 +12,14 @@ import rpg.api.gfx.IDrawable;
 import rpg.api.gfx.ImageUtility;
 
 public class Inventory implements IDrawable {
-	private static final BufferedImage HOTBAR = ImageUtility.scale(ResourceGetter.getImage("/assets/textures/overlay/inventory/hotbar.png"), Statics.scale * 1.7),
-			INVENTORY = ImageUtility.scale(ResourceGetter.getImage("/assets/textures/overlay/inventory/inventory.png"), Statics.scale * 1.7);
+	private static final int			MAX_INVENTORY_SIZE	= 16, HOTBAR_SLOTS = 4;
+	private static final BufferedImage	HOTBAR				= ImageUtility.scale(ResourceGetter.getImage("/assets/textures/overlay/inventory/hotbar.png"), Statics.scale * 1.7),
+			INVENTORY = ImageUtility.scale(ResourceGetter.getImage("/assets/textures/overlay/inventory/inventory.png"), Statics.scale * 1.7),
+			SLOT = HOTBAR.getSubimage(0, 0, HOTBAR.getWidth() / HOTBAR_SLOTS, HOTBAR.getWidth() / HOTBAR_SLOTS);
 	
-	private static final int MAX_INVENTORY_SIZE = 16, INVENTORY_WIDTH = 4;
+	private static final int W_INV = HOTBAR.getWidth(), H_INV = INVENTORY.getHeight(), H_HOT = HOTBAR.getHeight(),
+			X_INV = frameSize.width - W_INV, Y_HOT = frameSize.height - H_HOT, Y_INV = frameSize.height - H_INV - H_HOT,
+			SLOT_SIZE = W_INV / HOTBAR_SLOTS;
 	
 	public boolean						showFull	= false;
 	private final LinkedList<ItemStack>	questItems	= new LinkedList<>(), items = new LinkedList<>();
@@ -31,8 +35,8 @@ public class Inventory implements IDrawable {
 	 */
 	public boolean addItemStack(final ItemStack stack) {
 		if(stack.isQuestItem()) questItems.add(stack);
-		else if(items.size() >= MAX_INVENTORY_SIZE) return false;
-		else items.add(stack);
+		else if(items.size() < MAX_INVENTORY_SIZE) items.add(stack);
+		else return false;
 		
 		return true;
 	}
@@ -56,13 +60,43 @@ public class Inventory implements IDrawable {
 	}
 	
 	private void drawAlwaysVisible(final Graphics2D g2d) {
-		g2d.drawImage(HOTBAR, frameSize.width - HOTBAR.getWidth(), frameSize.height - HOTBAR.getHeight(), null);
+		g2d.drawImage(HOTBAR, X_INV, Y_HOT, null);
 		
-//		for(int i = 0; i < 4 && i < items.size(); i++) TODO: Draw Items
-//			items.get(i).draw(g2d, location);
+		for(int i = 0; i < HOTBAR_SLOTS && i < items.size(); i++) {
+			if(items.get(i) == null) continue;
+			final BufferedImage image = items.get(i).getSprite().getCurrentAnimationFrame();
+			
+			g2d.drawImage(image, X_INV + W_INV * i / HOTBAR_SLOTS, Y_HOT, null);
+		}
+		
+		g2d.drawImage(SLOT, 0, Y_HOT, null);
+		if(armorSlot != null) g2d.drawImage(armorSlot.getSprite().getCurrentAnimationFrame(), 0, Y_HOT, null);
+		
+		g2d.drawImage(SLOT, SLOT_SIZE, Y_HOT, null);
+		if(weaponSlot != null) g2d.drawImage(weaponSlot.getSprite().getCurrentAnimationFrame(), 0, Y_HOT, null);
 	}
 	
 	private void drawToggleVisible(final Graphics2D g2d) {
-		g2d.drawImage(INVENTORY, frameSize.width - INVENTORY.getWidth(), frameSize.height - HOTBAR.getHeight() - INVENTORY.getHeight(), null);
+		g2d.drawImage(INVENTORY, X_INV, Y_INV, null);
+		
+		for(int i = HOTBAR_SLOTS; i < items.size(); i++) {
+			if(items.get(i) == null) continue;
+			final BufferedImage image = items.get(i).getSprite().getCurrentAnimationFrame();
+			final int x = i % HOTBAR_SLOTS;
+			final int y = (i - x) / HOTBAR_SLOTS;
+			
+			g2d.drawImage(image, X_INV + SLOT_SIZE * x, Y_HOT - SLOT_SIZE * y, null);
+		}
+		
+		for(int i = 0; i < questItems.size(); i++) {
+			if(questItems.get(i) == null) continue;
+			
+			final BufferedImage image = questItems.get(i).getSprite().getCurrentAnimationFrame();
+			final int x = i % (HOTBAR_SLOTS + 1);
+			final int y = (i - x) / HOTBAR_SLOTS;
+			
+			g2d.drawImage(SLOT, Statics.frameSize.width - SLOT_SIZE * x, SLOT_SIZE * y, null);
+			g2d.drawImage(image, Statics.frameSize.width - SLOT_SIZE * x, SLOT_SIZE * y, null);
+		}
 	}
 }
